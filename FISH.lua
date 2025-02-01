700ca6d19e272fe8845ec2efb1eb6ff00a3ce478
Numerot <rph_326@hotmail.com>
2025-02-02 01:14:10 +0200

    Still working boiiiii

---
+++ FISH.lua
@@ -0,0 +1,60 @@
+FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+FIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIF
+FISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSIF
+FISHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHSIF
+FISH                                                    HSIF
+FISH                                                    HSIF
+FISH                                                    HSIF
+FISH                                                    HSIF
+FISH                                                    HSIF
+FISH     FF                                             HSIF
+FISH     F FFFFFFF     II       SSSSS                   HSIF
+FISH     F        FF   II     SS         H              HSIF
+FISH     F             II     S          H      H       HSIF
+FISH     F             II     SS         H      H       HSIF
+FISH     F F           II      SS        H      H       HSIF
+FISH     F  F          II        SSS     H      H       HSIF
+FISH     F   FFFFF     II          S     HHHHHHHH       HSIF
+FISH     F             II           S    H      H       HSIF
+FISH     F             II           S    H      H       HSIF
+FISH     F             II           S    H      H       HSIF
+FISH     F             II          SS    H      H       HSIF
+FISH     F             II      SSSS      H      H       HSIF
+FISH                                     H      H       HSIF
+FISH                                     H      H       HSIF
+FISH                                                    HSIF
+FISH                                                    HSIF
+FISH                        E                           HSIF
+FISH       AA                  N     D                  HSIF
+FISH       AA                 N      DDDDD              HSIF
+FISH       A AA         N     N      D    D             HSIF
+FISH      A    A       NN     N      D    DD            HSIF
+FISH      A    A       NN    N       D     D            HSIF
+FISH     AAAAAA A      NN    N        D    DD           HSIF
+FISH     A    A A     N  N   N        D    D            HSIF
+FISH    A        A    N  N  N         D    D            HSIF
+FISH    A        A   N   N  N         D   D             HSIF
+FISH    A        A   N    NN          D  D              HSIF
+FISH    A        A   N    N           DDD               HSIF
+FISH                                                    HSIF
+FISH                                                    HSIF
+FISH                                                    HSIF
+FISH       CCCC                                         HSIF
+FISH     CC        H             I    PPPP              HSIF
+FISH     C         H     H       I    P   P SSSSSS      HSIF
+FISH    C           H    H       I     P  P SS          HSIF
+FISH    C           H    H       I     P  P   SS        HSIF
+FISH    C           H    H       I     P  P    S        HSIF
+FISH    C           HHHHHH       I     PPPP    SS       HSIF
+FISH     C          H    H       I     P        S       HSIF
+FISH     C          H    H       I     P     SSSS       HSIF
+FISH      CC        H    H       I     P                HSIF
+FISH       CCC      H    H       II    P                HSIF
+FISH         C      H                                   HSIF
+FISH                                                    HSIF
+FISH                                                    HSIF
+FISH                                                    HSIF
+FISHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHSIF
+FISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSIF
+FIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIF
+FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
--- main.lua
+++ main.lua
@@ -5,11 +5,15 @@ end
 -- random variables and objects
 time = love.timer.getTime()
 moveTimer = 0
+-- set window size
+success = love.window.setMode (250, 250, {resizable=true})
 love.graphics.scale(10, 10)
-gridSize = 9
+gridSize = 11
 mainWorld = love.physics.newWorld (0, 0, false)
 charA = love.physics.newBody(mainWorld, gridSize*5, gridSize*5, "dynamic")
 moveTimeStamp = love.timer.getTime()
+bumpMap = {}
+bumpMapIterator = 0
 
 -- read, parse and print prefabs
 prefabPrinter = function (p, x, y)    
@@ -21,12 +25,10 @@ prefabPrinter = function (p, x, y)
         tbsLength = string.len(toBeSplit)
         counter = 0
         currentPrefabTable = {}
-        -- stores the characters into a table
+        -- stores the characters into a table; somewhere around here it craps out because it doesn't 
+        -- understand spaces from ASCII art made in REXpaint
         for counter = 0, tbsLength do
             currentSymbol = string.sub(toBeSplit, counter, counter)
-            if currentSymbol == " " then
-                currentSymbol = ""
-            end
             currentPrefabTable[counter] = currentSymbol
             counter = counter+1
         end
@@ -35,11 +37,18 @@ prefabPrinter = function (p, x, y)
         -- love.graphics.print(currentPrefabTable, x, y)
         -- print each character from the table individually
         for counter = 0, #currentPrefabTable do
+            -- mark the current XY position on the bumpmap
+            bumpMap.bumpMapIterator = drawX, drawY
+            bumpMapIterator = bumpMapIterator+1
+            if currentPrefabTable[counter] == "\n"
+                then
+                    drawY = drawY+gridSize
+                    drawX = x
+                end
             love.graphics.print(currentPrefabTable[counter], drawX, drawY)
             drawX = drawX+gridSize
             counter = counter+1
         end
-            
         counter = 0
         Char1 = string.sub(toBeSplit, 1, 1)
     end
@@ -51,7 +60,7 @@ function love.draw()
 --    love.graphics.circle("fill", 100, 100, 10)
 --    love.graphics.print("asd", 10, 0)
 --    love.graphics.print("qwerty", 10, 10)
-    love.graphics.print("A", charA:getX(), charA:getY())
+--    love.graphics.print("A", charA:getX(), charA:getY())
 --    love.graphics.print(time, 50, 50)
 --    love.graphics.print("_", 200, 200)
 --    love.graphics.print("_", 200, 215)
@@ -61,7 +70,22 @@ function love.draw()
 --    love.graphics.print("_", 245, 245)
 --    love.graphics.print("_", 260, 260)
 --    prefabPrinter("prefab1.lua", 10, 200)
-    prefabPrinter("SEH.lua", gridSize*10, gridSize*10)
+--    prefabPrinter("SEH.lua", gridSize*10, gridSize*10)
+-- Calculate the camera offset
+    local windowWidth, windowHeight = love.graphics.getDimensions()
+    local offsetX = windowWidth / 2 - charA:getX()
+    local offsetY = windowHeight / 2 - charA:getY()
+
+    -- Apply the camera translation
+    love.graphics.push()
+    love.graphics.translate(offsetX, offsetY)
+
+    -- Draw the scene
+    love.graphics.print("A", charA:getX(), charA:getY())
+    prefabPrinter("wideRoom.lua", gridSize * 0, gridSize * 0)
+
+    -- Reset the translation
+    love.graphics.pop()
 end
 
 -- movement
