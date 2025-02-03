a6de530d35621e20c4ffdc88d23bd1863b8a7ed0
Numerot <rph_326@hotmail.com>
2025-02-03 05:52:30 +0200

    dialogue box UI 0.1

---
+++ dialogueTree.lua
@@ -0,0 +1,7 @@
+You pressed space.
+You pressed space again.
+And again.
+And again...
+Wow!
+Good job!
+That spacebar is really getting some action.
--- main.lua
+++ main.lua
@@ -6,10 +6,9 @@ end
 grid = 11
 mainWorld = love.physics.newWorld (0, 0, false)
 -- player
-charA = love.physics.newBody(mainWorld, grid*5, grid*5, "dynamic")
+charA = love.physics.newBody(mainWorld, grid*3, grid*2, "dynamic")
 -- set window size
-success = love.window.setMode (250, 250, {resizable=true})
-
+success = love.window.setMode (500, 500, {resizable=true})
 
 -- RANDOM VARIABLES AND OBJECTS
 -- create bumpmap for collision detection
@@ -19,7 +18,30 @@ bumpMapIterator = 0
 time = love.timer.getTime()
 moveTimer = 0
 moveTimeStamp = love.timer.getTime()
+dialogueTimeStamp = love.timer.getTime()
 stepSound = love.audio.newSource("step.wav", "static")
+screenWidth, screenHeight = love.graphics.getWidth(), love.graphics.getHeight()
+currentDialogue = "Nothing special to comment on in the environment."
+
+dialogueTree = love.filesystem.newFile("dialogueTree.lua")
+dialogueTree:open("r")
+--dialogueList {}
+--for i=0, #tbsLength do
+    
+--end
+
+local uiCanvas = love.graphics.newCanvas(screenWidth, screenHeight/3)
+
+    -- draw coordinates function
+    function coDraw(x, y)
+        love.graphics.print({x,".",y}, grid*x, grid*y, 0, 0.5, 0.5)
+    end
+    -- specify coordinates to draw
+    for i = 0, 100 do
+        coDraw (i, i)
+        coDraw (i, 0)
+        coDraw (0, i)
+    end
 
 -- detect if table contains specific XY coordinates; ChatGPT's code
 function containsXY(tbl, xValue, yValue)
@@ -32,6 +54,15 @@ function containsXY(tbl, xValue, yValue)
         end
     end
     return false
+end
+
+function canMoveTo(x, y)
+    for _, obj in ipairs(bumpMap) do
+        if obj.X == x and obj.Y == y and obj.Char~=" " then
+            return false
+        end
+    end
+    return true
 end
 
 -- read, parse and print prefabs
@@ -67,7 +98,7 @@ function prefabPrinter(p, x, y)
                 end
             love.graphics.print(currentPrefabTable[counter], drawX, drawY)
             --if not currentPrefabTable[counter] == (" ") then
-                table.insert(bumpMap, {X=drawX, Y=drawY})
+                table.insert(bumpMap, {X=drawX, Y=drawY, Char=currentPrefabTable[counter]})
             --end
             drawX = drawX+grid
             counter = counter+1
@@ -80,85 +111,77 @@ end
 
 -- graphics
 function love.draw()
---    love.graphics.circle("fill", 100, 100, 10)
---    love.graphics.print("asd", 10, 0)
---    love.graphics.print("qwerty", 10, 10)
---    love.graphics.print("A", charA:getX(), charA:getY())
---    love.graphics.print(time, 50, 50)
---    love.graphics.print("_", 200, 200)
---    love.graphics.print("_", 200, 215)
---    love.graphics.print("_", 215, 200)
---    love.graphics.print("_", 215, 215)
---    love.graphics.print("_", 230, 230)
---    love.graphics.print("_", 245, 245)
---    love.graphics.print("_", 260, 260)
---    prefabPrinter("prefab1.lua", 10, 200)
---    prefabPrinter("SEH.lua", grid*10, grid*10)
-
-    --Calculate the camera offset
+    -- calculate the camera offset
     local windowWidth, windowHeight = love.graphics.getDimensions()
     local offsetX = windowWidth / 2 - charA:getX()
     local offsetY = windowHeight / 2 - charA:getY()
-
-    --Apply the camera translation
+    -- apply the camera translation
     love.graphics.push()
     love.graphics.translate(offsetX, offsetY)
 
-    -- Draw the scene
+    -- draw the scene
     love.graphics.print("A", charA:getX(), charA:getY())
-    -- prefabPrinter("wideRoom.lua", grid * 0, grid * 0)
-    --prefabPrinter("prefab1.lua", grid * 12, grid * 7)
 
     -- draw Kramnik
     kramnikSmall = love.graphics.newImage("kramnikSmall.jpg")
     love.graphics.draw(kramnikSmall, grid * 25, grid * 15)
-    prefabPrinter("wideROom.lua", grid*0, grid*0)
+    prefabPrinter("wideRoom.lua", grid*0, grid*0)
 
-    -- draw coordinates function
-    function coDraw(x, y)
-        love.graphics.print({x,".",y}, grid*x, grid*y, 0, 0.5, 0.5)
-    end
-    -- specify coordinates to draw
-    for i = 0, 100 do
---        coDraw (i, i)
---        coDraw (i, 0)
---        coDraw (0, i)
-    end
-
-    -- Reset the translation
+    -- reset the translation
     love.graphics.pop()
+
+    love.graphics.setCanvas(uiCanvas)
+    love.graphics.clear(0, 0, 0, 0) -- clear previous frame
+    love.graphics.setColor(0, 0, 0, 0.95)
+    love.graphics.rectangle("fill", 0, 0, 1000, 1000)
+    love.graphics.setColor(10, 10, 10)
+    prefabPrinter("borderD.lua", 0, screenHeight*2/3)
+    prefabPrinter("borderD.lua", 0, screenHeight-30)
+    prefabPrinter("borderDVert.lua", screenWidth+30, 0)
+    prefabPrinter("borderDVert.lua", screenWidth-10, 0)
+    love.graphics.print(currentDialogue, screenWidth/3, 50)
+    love.graphics.setCanvas()
+    
+    -- Draw the UI Canvas on the screen
+    love.graphics.draw(uiCanvas, 0, screenHeight*2/3)
 end
 
 -- movement
 function love.update()
     if love.timer.getTime() > moveTimeStamp+0.3 then
         if love.keyboard.isDown("right") then
-            if not containsXY(bumpMap, charA:getX() + grid, charA:getY()) then
+            if canMoveTo(charA:getX() + grid, charA:getY()) then
                 charA:setX(charA:getX() + grid)
                 love.audio.play(stepSound)
                 moveTimeStamp = love.timer.getTime()
             end
         end
         if love.keyboard.isDown("left") then
-            if not containsXY(bumpMap, charA:getX() - grid, charA:getY()) then
+            if canMoveTo(charA:getX() - grid, charA:getY()) then
                 charA:setX(charA:getX() - grid)
                 love.audio.play(stepSound)
                 moveTimeStamp = love.timer.getTime()
             end
         end
         if love.keyboard.isDown("up") then
-            if not containsXY(bumpMap, charA:getX(), charA:getY() - grid) then
+            if canMoveTo(charA:getX(), charA:getY() - grid) then
                 charA:setY(charA:getY() - grid)
                 love.audio.play(stepSound)
                 moveTimeStamp = love.timer.getTime()
             end
         end
         if love.keyboard.isDown("down") then
-            if not containsXY(bumpMap, charA:getX(), charA:getY() + grid) then
+            if canMoveTo(charA:getX(), charA:getY() + grid) then
                 charA:setY(charA:getY() + grid)
                 love.audio.play(stepSound)
                 moveTimeStamp = love.timer.getTime()
+            end
+        end
+        if love.timer.getTime() > dialogueTimeStamp+0.3 then
+            if love.keyboard.isDown("space") then
+                currentDialogue = "You pressed space."
+                dialogueTimeStamp = love.timer.getTime()
+            end
         end
     end
-end
 end
